5a21e11ec1f2bc4f596b371303d91563402b62f5
evvyj2001 <evvyj2001@naver.com>
2020-11-09 15:34:27 +0900

    dbconn.php 수정

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -1,5 +1,5 @@
 <?
-    $connect=mysql_connect( "localhost", "evvyj2001", "Dbwls1806!") or  
+    $connect=mysql_connect( "localhost", "---", "---") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
     mysql_select_db("evvyj2001",$connect);
